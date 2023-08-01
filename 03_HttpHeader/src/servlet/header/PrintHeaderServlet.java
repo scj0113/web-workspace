07e1f2f4227e7964e41f35c5c691ba11b63192f9
scj0113 <scj0113@naver.com>
2023-08-01 17:39:31 +0900

    a

---
+++ 03_HttpHeader/src/servlet/header/PrintHeaderServlet.java
@@ -0,0 +1,41 @@
+package servlet.header;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrintHeaderServlet
+ */
+public class PrintHeaderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+
+		
+		String id = request.getParameter("id");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h2>여기는 서비스 입니다...</h2>");
+		out.println("<p>ID : "+id+"</p>");
+		out.close();
+	}
+
+	
+
+	
+	
+	
+}
