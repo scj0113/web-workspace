45c315404e16f9100660861fa5e791f317435267
scj0113 <scj0113@naver.com>
2023-08-02 15:10:43 +0900

    a

--- 03_HttpHeader/src/servlet/header/PrintHeaderServlet2.java
+++ 03_HttpHeader/src/servlet/header/PrintHeaderServlet2.java
@@ -43,7 +43,12 @@ public class PrintHeaderServlet2 extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-//		doGet(request, response);
+		doGet(request, response);
+		
+		
+		
+		
+		
 	}
 
 }
